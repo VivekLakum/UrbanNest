3c80f3244f337fb02510eb3eac7933090628d30e
Vivek Lakum <lakumvivek01@gmail.com>
2026-01-25 18:46:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1cdxb4JSn17UPlfGWF6ih1A5FBqsahJkU
 """
 
-!pip install streamlit
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
